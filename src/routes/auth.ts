8f5c080cbd4c1d4cd483d99bbb9ee91ca5923661
Jace <aleshko.vitali@gmail.com>
2020-03-17 23:46:39 +0300

    reg

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,77 @@
 import { Router } from 'express';
+import { isObject, isString, isNumber } from 'util';
+
+import { Users } from '../models/Users';
 
 const app = Router();
 
+interface RegForm {
+  name: string;
+  surname: string;
+  country: string;
+  city: string;
+
+  height: number;
+  weight: number;
+  birthday: number;
+
+  hairColor: string;
+  eyesColor: string;
+  telefon: string;
+  email: string;
+  password: string;
+}
+
+const isRegForm = (data: RegForm): data is RegForm => (
+  isObject(data) &&
+
+  isString(data.name) &&
+  isString(data.surname) &&
+  isString(data.country) &&
+  isString(data.city) &&
+
+  isNumber(data.height) &&
+  isNumber(data.weight) &&
+  isNumber(data.birthday) &&
+
+  isString(data.hairColor) &&
+  isString(data.eyesColor) &&
+  isString(data.telefon) &&
+  isString(data.email) &&
+  isString(data.password)
+);
+
 app.post('/reg', async (req, res) => {
+  const regForm: RegForm = req.body;
+
+  if (!isRegForm(regForm)) {
+    throw new Error();
+  }
+
+  await req.database.manager.insert(Users, regForm);
+
   res.end();
 });
 
 export default app;
+
+
+
+// TEST
+
+const test: RegForm = {
+  "name": "",
+  "surname": "",
+  "country": "",
+  "city": "",
+
+  "height": 0,
+  "weight": 0,
+  "birthday": 0,
+
+  "hairColor": "",
+  "eyesColor": "",
+  "telefon": "",
+  "email": "",
+  "password": ""
+};
